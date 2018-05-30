519a905de7dae9aa7a421996a072e2058f1d9d09
vlad <vlad.miakshin@gmail.com>
2018-05-30 18:06:39 +0300

    added some tests

--- server/main.go
+++ server/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
     a := App{}
     // You need to set your Username and Password here
-    a.Initialize("DB_USERNAME", "DB_PASSWORD", "rest_api_example")
+    a.Initialize("admin", "admin", "rest_api_example")
 
     a.Run(":8080")
 }
--- server/main_test.go
+++ server/main_test.go
@@ -3,6 +3,9 @@ import (
     "os"
     "log"
     "testing"
+    "net/http"
+    "net/http/httptest"
+    "encoding/json"
 )
 var a App
 func TestMain(m *testing.M) {
@@ -30,3 +33,61 @@ CREATE TABLE IF NOT EXISTS users
     login VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL
 )`
+func TestEmptyTable(t *testing.T) {
+    clearTable()
+    req, _ := http.NewRequest("GET", "/users", nil)
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusOK, response.Code)
+    if body := response.Body.String(); body != "[]" {
+        t.Errorf("Expected an empty array. Got %s", body)
+    }
+}
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+    rr := httptest.NewRecorder()
+    a.Router.ServeHTTP(rr, req)
+
+    return rr
+}
+func checkResponseCode(t *testing.T, expected, actual int) {
+    if expected != actual {
+        t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+    }
+}
+func TestGetNonExistentUser(t *testing.T) {
+    clearTable()
+    req, _ := http.NewRequest("GET", "/user/45", nil)
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusNotFound, response.Code)
+    var m map[string]string
+    json.Unmarshal(response.Body.Bytes(), &m)
+    if m["error"] != "User not found" {
+        t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+    }
+}
+func TestCreateUser(t *testing.T) {
+    clearTable()
+    payload := []byte(`{"name":"test user","age":30}`)
+    req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusCreated, response.Code)
+    var m map[string]interface{}
+    json.Unmarshal(response.Body.Bytes(), &m)
+    if m["name"] != "test user" {
+        t.Errorf("Expected user name to be 'test user'. Got '%v'", m["name"])
+    }
+    if m["age"] != 30.0 {
+        t.Errorf("Expected user age to be '30'. Got '%v'", m["age"])
+    }
+    // the id is compared to 1.0 because JSON unmarshaling converts numbers to
+    // floats, when the target is a map[string]interface{}
+    if m["id"] != 1.0 {
+        t.Errorf("Expected user ID to be '1'. Got '%v'", m["id"])
+    }
+}
+func TestGetUser(t *testing.T) {
+    clearTable()
+    addUsers(1)
+    req, _ := http.NewRequest("GET", "/user/1", nil)
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusOK, response.Code)
+}
